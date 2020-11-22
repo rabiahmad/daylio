9b92680dc81aa1c4c292e27732622928a979c2c5
Rabi Ahmad <rabihassanahmad@gmail.com>
2020-11-22 21:05:08 +0000

    chart added: mood variance over time

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from dash.dependencies import Input, Output
 from datetime import datetime
 from datetime import date
 
+import numpy as np
 
 # import data
 df = pd.read_excel("daylio.xlsx", sheet_name="data")
@@ -17,17 +18,24 @@ moods = pd.read_excel("daylio.xlsx", sheet_name="moods")
 # clean data
 df["full_date"] = pd.to_datetime(df["full_date"])
 
-# aggregate data
+# average mood over time
 aggregations = {"mood value": "mean"}
-df_avgmood = df.groupby(by=["full_date", "name"]).agg(
-    aggregations).reset_index()
+df_avgmood = df.groupby(by=["full_date", "name"])\
+    .agg(aggregations)\
+    .reset_index()\
+    .rename(columns={'mood value': 'average mood'})
 
+
+# variance of average mood over time between all members of the "name" class
+aggregations = {"average mood": np.var}
+df_mood_variance = df_avgmood.groupby("full_date")\
+    .agg(aggregations)\
+    .reset_index()\
+    .rename(columns={'average mood': 'mood variance'})
 
 # ------------------------------------------------------------------------
 # Initialise App
-external_stylesheets = ['https://codepen.io/anon/pen/mardKv.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.H1("Daylio", style={"text-align": "center"}),
@@ -45,7 +53,9 @@ app.layout = html.Div([
 
     html.Div(id='output-container-date-picker-range'),
 
-    dcc.Graph(id="average-mood-over-time", figure={})
+    dcc.Graph(id="average-mood-over-time", figure={}),
+
+    dcc.Graph(id="mood-variance-over-time", figure={}),
 ])
 
 # ------------------------------------------------------------------------
@@ -56,16 +66,41 @@ app.layout = html.Div([
     [Input(component_id="my-date-picker-range", component_property="start_date"),
      Input(component_id="my-date-picker-range", component_property="end_date")]
 )
-def update_chart(start_date, end_date):
+def update_chart1(start_date, end_date):
     # print(start_date, type(start_date))
     # print(end_date, type(end_date))
 
-    df_avgmood_copy = df_avgmood.copy()
-    df_avgmood_copy = df_avgmood_copy[(df_avgmood_copy["full_date"] >= start_date) & (
-        df_avgmood_copy["full_date"] <= end_date)]
+    data_copy = df_avgmood.copy()
+    data_copy = data_copy[(data_copy["full_date"] >= start_date) & (
+        data_copy["full_date"] <= end_date)]
 
-    figure = px.line(df_avgmood_copy, x="full_date",
-                     y="mood value", color='name')
+    figure = px.line(data_copy, x="full_date", y="average mood", color='name')
+
+    figure.update_layout(legend=dict(
+        yanchor="bottom",
+        y=0.80,
+        xanchor="left",
+        x=0.01
+    ))
+
+    figure.layout.template = 'simple_white'
+
+    return figure
+
+
+@app.callback(
+    Output(component_id="mood-variance-over-time",
+           component_property="figure"),
+    [Input(component_id="my-date-picker-range", component_property="start_date"),
+     Input(component_id="my-date-picker-range", component_property="end_date")]
+)
+def update_chart2(start_date, end_date):
+    data_copy = df_mood_variance.copy()
+    data_copy = data_copy[(data_copy["full_date"] >= start_date) & (
+        data_copy["full_date"] <= end_date)]
+
+    figure = px.line(data_copy, x="full_date", y="mood variance")
+    figure.layout.template = 'simple_white'
     return figure
 
 
